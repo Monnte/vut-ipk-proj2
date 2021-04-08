acd82450c6062a7521c73cb3c4a04632b32fd8d8
Peter Zdravecký <zdravecky.zdravecky@gmail.com>
2021-04-08 14:52:31 +0200

    Sniff sniff

--- main.cpp
+++ main.cpp
@@ -6,13 +6,13 @@ int main(int argc, char **argv)
 	/* Arugment Parsing */
 	/** @see https://www.gnu.org/software/libc/manual/html_node/Getopt.html */
 
-	std::string interface = "eth0";
+	char *interface = nullptr;
 	int udp = 0;
 	int tcp = 0;
 	int arp = 0;
 	int icmp = 0;
-	int port = 23;
-	int num = 10;
+	int port = 0;
+	long num = 1;
 	char *ptrEnd[2];
 
 	static struct option long_options[] =
@@ -21,13 +21,14 @@ int main(int argc, char **argv)
 			{"tcp", no_argument, &tcp, 1},
 			{"arp", no_argument, &arp, 1},
 			{"icmp", no_argument, &icmp, 1},
-			{"interface", required_argument, nullptr, 'i'},
+			{"interface", optional_argument, nullptr, 'i'},
+			{"help", no_argument, nullptr, 'h'},
 			{0, 0, 0, 0},
 
 		};
 
 	int c;
-	while ((c = getopt_long(argc, argv, "uti::p:n:", long_options, nullptr)) != -1)
+	while ((c = getopt_long(argc, argv, "uti::p:n:h", long_options, nullptr)) != -1)
 	{
 		switch (c)
 		{
@@ -45,7 +46,7 @@ int main(int argc, char **argv)
 			if (!optarg && optind < argc && NULL != argv[optind] && '\0' != argv[optind][0] && '-' != argv[optind][0])
 				interface = argv[optind++];
 			else if (!optarg)
-				return printInterfaces();
+				return print_interfaces();
 			else
 				interface = optarg;
 			break;
@@ -53,7 +54,7 @@ int main(int argc, char **argv)
 			port = std::strtol(optarg, &ptrEnd[0], 10);
 			if (*ptrEnd[0] != '\0')
 			{
-				std::cout << "-p argument must be integer" << std::endl;
+				fprintf(stderr, "-p argument must be integer\n");
 				return 1;
 			}
 			break;
@@ -61,9 +62,13 @@ int main(int argc, char **argv)
 			num = std::strtol(optarg, &ptrEnd[1], 10);
 			if (*ptrEnd[1] != '\0')
 			{
-				std::cout << "-n argument must be integer" << std::endl;
+				fprintf(stderr, "-n argument must be integer\n");
 				return 1;
 			}
+			break;
+		case 'h':
+			print_help();
+			return 0;
 			break;
 		case '?':
 			return 1;
@@ -73,29 +78,168 @@ int main(int argc, char **argv)
 		}
 	}
 
-	if (!udp && !tcp && !arp && !icmp)
+	if (!interface)
 	{
-		std::cout << "[--tcp|-t] [--udp|-u] [--arp] [--icmp] at least one option is requried" << std::endl;
+		return print_interfaces();
+	}
+	if ((udp + tcp + arp + icmp) > 1)
+	{
+		fprintf(stderr, "Error: [--tcp|-t] [--udp|-u] [--arp] [--icmp] max one option\n");
+		return 1;
+	}
+	if (port > 65535 || port < 0)
+	{
+		fprintf(stderr, "Error: port is out of range valid ports\n");
 		return 1;
 	}
 
-	std::cout << "interface: " << interface << std::endl;
-	printf("udp: %d\n", udp);
-	printf("tcp: %d\n", tcp);
-	printf("arp: %d\n", arp);
-	printf("icmp: %d\n", icmp);
-	printf("port: %d\n", port);
-	printf("num: %d\n", num);
+	char errMsg[PCAP_ERRBUF_SIZE];
+
+	bpf_u_int32 ip, maskp;
+	if (pcap_lookupnet(interface, &ip, &maskp, errMsg) == -1)
+	{
+		printf("Error: %s\n", errMsg);
+		return 1;
+	}
+
+	pcap_t *handle;
+	int timeout = 10000;
+
+	handle = pcap_open_live(interface, BUFSIZ, 1, timeout, errMsg);
+	if (!handle)
+	{
+		printf("Error: %s\n", errMsg);
+		return 1;
+	}
+
+	struct bpf_program fp;
+	char filter[200];
+
+	if (port)
+	{
+		char portStr[20];
+		sprintf(portStr, "%d", port);
+		strcpy(filter, "port ");
+		strcat(filter, portStr);
+	}
+
+	if (tcp)
+		strcat(filter, " and tcp");
+	if (udp)
+		strcat(filter, " and udp");
+	if (icmp)
+		strcat(filter, " and icmp");
+	if (arp)
+		strcat(filter, " and arp");
+
+	printf("filter: %s\n", filter);
+
+	if (pcap_compile(handle, &fp, filter, 0, ip) == -1)
+	{
+		fprintf(stderr, "Error: %s\n", pcap_geterr(handle));
+		return 1;
+	}
+
+	if (pcap_setfilter(handle, &fp) == -1)
+	{
+		fprintf(stderr, "Error: %s\n", pcap_geterr(handle));
+		return 1;
+	}
+
+	if (pcap_loop(handle, num, pkt_handle, NULL) < 0)
+	{
+		fprintf(stderr, "Error: error occures while capturing packets\n");
+		return 1;
+	}
+	pcap_freecode(&fp);
+	pcap_close(handle);
+	return 0;
 }
 
-int printInterfaces()
+void pkt_handle(u_char *user, const struct pcap_pkthdr *pkt_header, const u_char *pkt_data)
 {
-	char error[PCAP_ERRBUF_SIZE];
+	struct iphdr *iph = (struct iphdr *)(pkt_data + sizeof(struct ethhdr));
+	switch (iph->protocol) //Check the Protocol and do accordingly...
+	{
+	case 1: //ICMP Protocol
+		printf("ICMP");
+		break;
+
+	case 6: //TCP Protocol
+		printf("TCP");
+		break;
+
+	case 17: //UDP Protocol
+		printf("UDP");
+		break;
+	case 54:
+		printf("ARP");
+		break;
+	case 58: //ICMP for IPv6
+		printf("ICMP IPV6");
+		break;
+	default:
+		break;
+	}
+}
+
+void print_packet(const u_char *pkt_data, int size)
+{
+	int i;
+	for (i = 0; i < size; i++)
+	{
+		if (i % 16 == 0)
+			printf("\n");
+
+		if (i != 0 && i % 16 == 0)
+		{
+			for (int j = i - 16; j < i; j++)
+			{
+
+				printf("%02X ", pkt_data[j]);
+			}
+
+			for (int j = i - 16; j < i; j++)
+			{
+				if (j % 8 == 0)
+					printf(" ");
+				if (isprint(pkt_data[j]))
+					printf("%c", pkt_data[j]);
+				else
+					printf(".");
+			}
+		}
+	}
+
+	//print restt of packet
+	printf("\n");
+	for (int j = i - i % 16; j < i; j++)
+	{
+		printf("%02X ", pkt_data[j]);
+	}
+	for (int j = i % 16; j < 16; j++)
+		printf("   ");
+	for (int j = i - i % 16; j < i; j++)
+	{
+		if (j % 8 == 0)
+			printf(" ");
+		if (isprint(pkt_data[j]))
+			printf("%c", pkt_data[j]);
+		else
+			printf(".");
+	}
+
+	printf("\n\n");
+}
+
+int print_interfaces()
+{
+	char errMsg[PCAP_ERRBUF_SIZE];
 	pcap_if_t *interfaces, *interface;
 
-	if (pcap_findalldevs(&interfaces, error) == -1)
+	if (pcap_findalldevs(&interfaces, errMsg) == -1)
 	{
-		printf("Error: %s\n", error);
+		printf("Error: %s\n", errMsg);
 		return 1;
 	}
 
@@ -105,4 +249,32 @@ int printInterfaces()
 	}
 
 	return 0;
+}
+
+void print_help()
+{
+	printf("Packet sniffer v0.1\n");
+	printf("Usage:");
+	printf("  ./ipk-sniffer [-i rozhraní | --interface rozhraní] {-p port} {[--tcp|-t] [--udp|-u] [--arp] [--icmp] } {-n num}\n");
+	printf("Author: Peter Zdravecký\n");
+}
+
+void printTimeRFC3339(struct pcap_pkthdr pkt_header)
+{
+	struct timeval tv = pkt_header.ts;
+	struct tm *tm;
+	char time[100];
+	char timebuffer[50];
+
+	tm = localtime(&tv.tv_sec);
+
+	strftime(time, 26, "%Y-%m-%dT%H:%M:%S", tm);
+	char sign = '+';
+	if (tm->tm_gmtoff < 0)
+	{
+		sign = '-';
+	}
+	sprintf(timebuffer, ".%03d%c%02d:%02ld", int(round(tv.tv_usec / 1000)), sign, int(abs(tm->tm_gmtoff) / 3600), (abs(tm->tm_gmtoff) / 60) % 60);
+	strcat(time, timebuffer);
+	printf("%s", time);
 }
--- main.h
+++ main.h
@@ -3,11 +3,30 @@
 #include <stdlib.h>
 #include <getopt.h>
 #include <string>
+#include <string.h>
+#include <time.h>
+#include <math.h>
+
+#include <netinet/udp.h>
+#include <netinet/tcp.h>
+#include <netinet/icmp6.h>
+#include <netinet/ip_icmp.h>
+
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <net/ethernet.h>
 
 /** @see http://www.ferrisxu.com/WinPcap/html/funcs_2pcap_8h.html 
- *  @see https://www.devdungeon.com/content/using-libpcap-c
- *  @see 
 */
 #include <pcap/pcap.h>
 
-int printInterfaces();
+/** TIME
+ * @see https://tools.ietf.org/html/rfc3339#section-5.8
+ * 
+*/
+
+void print_help();
+int print_interfaces();
+void printTimeRFC3339(struct pcap_pkthdr pkt_header);
+void print_packet(const u_char *pkt_data, int size);
+void pkt_handle(u_char *user, const struct pcap_pkthdr *pkt_header, const u_char *pkt_data);
